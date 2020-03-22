019ef4c9e45b11ef0409f79dbc6872eefa30db5b
Nastya <nastya_mi@icloud.com>
2020-03-22 14:57:04 +0300

    feat: reverse-int task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let splitStr = str.split("");
+    let reverseArr = splitStr.reverse().join("");
+    let reverseStr = parseInt(reverseArr);
+    return reverseStr;
 }
